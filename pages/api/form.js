4f9ff4ad7fea582e500e7b6c7e237d971fe7b751
Juliette Audema <ajuliette.dev@gmail.com>
2023-06-10 17:16:33 +0200

    Add inputs

--- pages/api/form.js
+++ pages/api/form.js
@@ -4,18 +4,28 @@ export default function handler(req, res) {
 
     // Optional logging to see the responses
     // in the command line where next.js app is running.
-    console.log('CITY')
-    console.log(body)
+    console.log('CITY');
+    console.log(body);
 
+    const jobs = showMeData();
 
-    // Guard clause checks for first and last name,
-    // and returns early if they are not found
-    if (!body.city) {
+    if (!jobs) {
         // Sends a HTTP bad request error code
-        return res.status(400).json({ data: 'Data not found' })
-    }
-
-    // Found the name.
-    // Sends a HTTP success code
-    res.status(200).json({ data: `${body.city} ${body.categories} ${body.revenue_range} ${body.red_flags}` })
+        return res.status(400).json({ data: 'No jobs found :(' })
+      }
+     
+      // Found the name.
+      // Sends a HTTP success code
+      res.status(200).json({ data: `${jobs}` })
 }
+
+async function showMeData() {
+    const getData = async () => {
+        const response = await fetch('https://shesharpnl.github.io/hackathon-2023.sourcestack-data/assets/junior-nl-all-fields.json');
+        const data = await response.json();
+        console.log(data)
+        return data;
+    }
+    let jsonData = await getData();
+}
+
