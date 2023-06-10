143b42f48ffe050c953306c308c2e19d1db96564
Juliette Audema <ajuliette.dev@gmail.com>
2023-06-10 17:38:33 +0200

    Call API instead of JSON

--- pages/api/form.js
+++ pages/api/form.js
@@ -8,7 +8,14 @@ export default async function handler(req, res) {
 }
 
 async function getAPIData() {
-    return fetch('https://shesharpnl.github.io/hackathon-2023.sourcestack-data/assets/junior-nl-all-fields.json')
+    return fetch('https://148581a8-8e6e-4837-a77e-d8733cf62e83.mock.pstmn.io/jobs',
+        {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+          }
+        })
         .then(response => response.json())
         .then(json => {
         // Found the name.
